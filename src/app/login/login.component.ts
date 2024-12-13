19d79b1467c4151df77282ee912345196564dd95
Aeroxflush <ragielgerorius@gmail.com>
2024-12-13 11:39:03 +0700

    PERLU PERBAIKAN
    Logout belum berfungsi
    
    onSignButtonClick() perlu dirubah

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,25 +1,72 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, Renderer2 } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Router } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
+import { subscribeOn } from 'rxjs';
+
+declare const $: any;
 
 @Component({
-    selector: 'app-login',
-    imports: [RouterModule],
-    templateUrl: './login.component.html',
-    styleUrl: './login.component.css'
+  selector: 'app-login',
+  imports: [RouterModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
-  constructor(private renderer: Renderer2) {
-    this.renderer.addClass(document.body, "login-page")
+  constructor(
+    private renderer: Renderer2,
+    private httpClient: HttpClient,
+    private router: Router,
+    private cookieService: CookieService
+  ) {
+    this.renderer.addClass(document.body, 'login-page');
 
-    this.renderer.removeClass(document.body, "sidebar-mini")
-    this.renderer.removeClass(document.body, "layout-fixed")
+    this.renderer.removeClass(document.body, 'sidebar-mini');
+    this.renderer.removeClass(document.body, 'layout-fixed');
 
-    this.renderer.setAttribute(document.body, "style", "min-height: 464px;")
+    this.renderer.setAttribute(document.body, 'style', 'min-height: 464px;');
   }
 
   onSignButtonClick() {
     const body = document.body;
     this.renderer.removeClass(body, 'login-page');
     this.renderer.setStyle(body, 'min-height', 'auto');
+  }
+
+  showPeringatanModal(message: string): void {
+    $('#peringatanModal').modal();
+    $('#pm_message').html(message);
+  }
+
+  signIn(): void {
+    console.log('signIn()');
+
+    var userId = $('#idText').val();
+    userId = encodeURIComponent(userId);
+
+    var password = $('#passwordText').val();
+    password = encodeURIComponent(password);
+
+    var url =
+      'https://stmikpontianak.cloud/011100862/login.php' +
+      '?id=' +
+      userId +
+      '&password=' +
+      password;
+    console.log('url : ' + url);
+
+    this.httpClient.get(url).subscribe((data: any) => {
+      console.log(data);
+      var row = data[0];
+
+      if (row.idCount != '1') {
+        this.showPeringatanModal('Id atau password tidak cocok');
+        return;
+      }
+
+      this.cookieService.set('userId', userId);
+      console.log('session data berhasil dibuat');
+      this.router.navigate(['/dashboard']);
+    });
   }
 }
---
+++ src/app/logout/logout.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
+
+@Component({
+  selector: 'app-logout',
+  templateUrl: './logout.component.html',
+  styleUrl: './logout.component.css',
+})
+export class LogoutComponent {
+  constructor(private cookieService: CookieService, private router: Router) {}
+
+  ngOnInit(): void {
+    this.cookieService.deleteAll();
+    this.router.navigate(['/login']);
+  }
+}
