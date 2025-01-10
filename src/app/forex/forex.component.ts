7826d719bd7afa85878cab9fd630f1c2f13bb729
Aeroxflush <ragielgerorius@gmail.com>
2025-01-10 19:55:46 +0700

    update
    fix tabel cuaca dan update tabel forex
    
    login page masih perlu perbaikan

--- src/app/forex/forex.component.ts
+++ src/app/forex/forex.component.ts
@@ -3,13 +3,14 @@ import { HeaderComponent } from "../header/header.component";
 import { SidebarComponent } from "../sidebar/sidebar.component";
 import { HttpClient } from '@angular/common/http';
 import { formatCurrency } from '@angular/common';
+import { FooterComponent } from '../footer/footer.component';
 
 declare const $ : any;
 
 @Component({
   selector: 'app-forex',
   standalone: true,
-  imports: [HeaderComponent, SidebarComponent],
+  imports: [HeaderComponent, SidebarComponent, FooterComponent],
   templateUrl: './forex.component.html',
   styleUrl: './forex.component.css'
 })
@@ -27,7 +28,7 @@ export class ForexComponent implements AfterViewInit{
     this._table1 = $("#table1").DataTable({
       "columnDefs": [
         {
-          "targets": 2,
+          "targets": 3,
           "className": "text-right"
         }
       ],
@@ -36,27 +37,56 @@ export class ForexComponent implements AfterViewInit{
     this.bindTable1();
   }
 
-    bindTable1(): void {
-      console.log("bindTable1()");
+    // bindTable1(): void {
+    //   console.log("bindTable1()");
       
-      var url = "https://openexchangerates.org/api/latest.json?app_id=d7c1514b62f74fdeb17ccaa27ea45aef";
+    //   var url = "https://openexchangerates.org/api/latest.json?app_id=d7c1514b62f74fdeb17ccaa27ea45aef";
 
-      this.httpClient.get(url).subscribe((data: any) => {
-        var rates = data.rates;
-        console.log(rates);
+    //   this.httpClient.get(url).subscribe((data: any) => {
+    //     var rates = data.rates;
+    //     console.log(rates);
 
+    //     let index = 1;
+
+    //     for (const currency in rates){
+    //       const rate = rates.IDR / rates[currency];
+    //       const formatrate = formatCurrency(rate, "en-US", "", currency);
+    //       console.log('${currency} : ${formatrate}');
+
+    //       const row = [index++, currency, formatrate];
+    //       this._table1.row.add(row);
+    //     }
+
+    //     this._table1.draw(false);
+    //   });
+    // }
+
+  // BARU
+  bindTable1(): void {
+    console.log("bindTable1()");
+      
+    const ratesUrl = "https://openexchangerates.org/api/latest.json?app_id=d7c1514b62f74fdeb17ccaa27ea45aef";
+
+    const currenciesUrl = "https://openexchangerates.org/api/currencies.json";
+
+    this.httpClient.get(currenciesUrl).subscribe((currencies: any) => {
+      this.httpClient.get(ratesUrl).subscribe((data: any) => {
+        const rates = data.rates;
         let index = 1;
 
         for (const currency in rates){
+          const currencyName = currencies[currency];
+
           const rate = rates.IDR / rates[currency];
-          const formatrate = formatCurrency(rate, "en-US", "", currency);
-          console.log('${currency} : ${formatrate}');
+          const formatRate = formatCurrency(rate, "en-US", "", currency);
 
-          const row = [index++, currency, formatrate];
+          console.log(`${currency}: ${currencyName} - ${formatRate}`);
+
+          const row = [index++, currency, currencyName, formatRate];
           this._table1.row.add(row);
+          this._table1.draw(false);
         }
-
-        this._table1.draw(false);
       });
-    }
+    });
+  }
 }
